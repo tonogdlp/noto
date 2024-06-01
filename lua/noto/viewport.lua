8523e1287f48c73c304afa64cf9dfb0616af3b18
tonogdlp <8254361+tonogdlp@users.noreply.github.com>
2024-06-01 00:12:52 -0700

    [refactor] Changed M = {} for Viewport = {}

--- lua/noto/viewport.lua
+++ lua/noto/viewport.lua
@@ -1,16 +1,16 @@
-local M = {}
+local Viewport = {}
 --- Viewport is the window that shows you a Noto DB.
 ---
 ---
 
-M.vp_autogroup = vim.api.nvim_create_augroup("Noto.Viewport.Autogroup", {})
+Viewport.vp_autogroup = vim.api.nvim_create_augroup("Noto.Viewport.Autogroup", {})
 
 ---@class Event
 ---@field buf number
 ---@field event string
 
-M.buf_id = nil
-M.win_id = nil
+Viewport.buf_id = nil
+Viewport.win_id = nil
 ---@class LineRange
 ---@field x_i number
 ---@field x_f number
@@ -27,12 +27,12 @@ M.win_id = nil
 ---@field style string
 
 ---@type LineRange
-M.db_location = {
+Viewport.db_location = {
 	x_i = 10,
 	x_f = 12,
 }
 
-M.in_viewport = false
+Viewport.in_viewport = false
 
 -- { 10, 12 }
 
@@ -40,41 +40,41 @@ M.in_viewport = false
 ---
 
 ---@param cursor_pos CursorPosition
-function M.is_inside_db_location(cursor_pos)
-	if cursor_pos[1] >= M.db_location.x_i and cursor_pos[1] <= M.db_location.x_f then
+function Viewport.line_is_notodb(cursor_pos)
+	if cursor_pos[1] >= Viewport.db_location.x_i and cursor_pos[1] <= Viewport.db_location.x_f then
 		return true
 	else
 		return false
 	end
 end
 
-function M.t()
+function Viewport.t()
 	print(1)
 end
 
-function M.replace_dbmarks_in_file(filepath)
-	--
+function Viewport.replace_dbmarks_in_file(filepath)
+	--:
 end
 
--- DONE: Detect when entering Viewport
-function M.cursor_moved_in_file_with_db()
+-- DONE Detect when entering Viewport
+function Viewport.cursor_moved_in_file_with_db()
 	-- Autocmd --> when location changed, calculate if inside DB
 
 	vim.api.nvim_create_autocmd("CursorMoved", {
-		group = M.vp_autogroup,
+		group = Viewport.vp_autogroup,
 		pattern = "*.testdb",
 		callback = function()
 			local cur = vim.api.nvim_win_get_cursor(0)
-			local is_inside_db = M.is_inside_db_location(cur)
+			local is_inside_db = Viewport.line_is_notodb(cur)
 			if is_inside_db then
-				if not M.in_viewport then
-					M.in_viewport = true
-					M.db_viewport_entered(cur)
+				if not Viewport.in_viewport then
+					Viewport.in_viewport = true
+					Viewport.db_viewport_entered(cur)
 				end
 			else
-				if M.in_viewport then
-					M.in_viewport = false
-					M.db_viewport_exited()
+				if Viewport.in_viewport then
+					Viewport.in_viewport = false
+					Viewport.db_viewport_exited()
 				end
 			end
 		end,
@@ -82,9 +82,10 @@ function M.cursor_moved_in_file_with_db()
 end
 
 ---@param cursor_pos CursorPosition
-function M.create_window_config(cursor_pos)
+function Viewport.create_window_config(cursor_pos)
+	-- test
 	-- local ui = vim.api.nvim_list_uis()[1]
-	print("x: " .. cursor_pos[1] .. ", y: " .. cursor_pos[2])
+	print("x " .. cursor_pos[1] .. ", y: " .. cursor_pos[2])
 
 	---@type WindowConfig
 	local opts = {
@@ -103,7 +104,7 @@ end
 
 ---
 ---@param cursor_pos CursorPosition
-function M.db_viewport_entered(cursor_pos)
+function Viewport.db_viewport_entered(cursor_pos)
 	print("viewport entered")
 
 	local buf_id = vim.api.nvim_create_buf(false, true)
@@ -111,24 +112,24 @@ function M.db_viewport_entered(cursor_pos)
 	-- vim.api.nvim_buf_set_name(buf_id, "../../examples/db_example1.notodb")
 
 	vim.fn.bufload(buf_id)
-	local config = M.create_window_config(cursor_pos)
+	local config = Viewport.create_window_config(cursor_pos)
 	local win_id = vim.api.nvim_open_win(buf_id, true, config)
 
 	print("BUF_id --> " .. buf_id)
-	M.buf_id = buf_id
-	M.win_id = win_id
+	Viewport.buf_id = buf_id
+	Viewport.win_id = win_id
 end
 
 --
 
 -- TODO: Detect when exiting Viewport
-function M.db_viewport_exited()
+function Viewport.db_viewport_exited()
 	--
 	print("viewport exited")
-	M.buf_id = nil
-	M.win_id = nil
+	Viewport.buf_id = nil
+	Viewport.win_id = nil
 end
 
-M.cursor_moved_in_file_with_db()
+Viewport.cursor_moved_in_file_with_db()
 
-return M
+return Viewport
